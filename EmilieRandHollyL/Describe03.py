3752950adc9d6fc50e1ea7805c21090909c72119
Emilie Rabeau <rabe0019@algonquinlive.com>
2018-01-16 17:14:43 -0500

    describe does not work :(

---
+++ EmilieRandHollyL/Describe03.py
@@ -0,0 +1,18 @@
+import os
+import arcpy
+import sys
+from arcpy import env
+
+scriptPath = os.path.dirname(__file__)
+os.chdir(scriptPath)
+
+fc = r"..\..\..\Data\Canada\province.shp"
+
+if len(sys.argv[fc]): =! 2:
+    print "error message"
+else:
+    print
+
+else:
+
+
