25cad72b563405128739ad4ee044ea46e6317cb9
DESKTOP-A217FHO\holly <long0165@algonquinlive.com>
2018-01-16 16:44:19 -0500

    moved data, d 1 and d2

--- EmilieRandHollyL/Describe01.py
+++ EmilieRandHollyL/Describe01.py
@@ -4,9 +4,9 @@ from arcpy import env
 
 scriptPath = os.path.dirname(__file__)
 os.chdir(scriptPath)
-env.workspace = r"..\..\..\..\acgis\gis4207_Customization_1\day02\lab\Data\Canada"
-fc = r"C:\Users\mie_r\Documents\acgis\gis4207_Customization_1\day02\lab\Data\Canada\province.shp"
-dsFc = arcpy.Describe(fc)
+env.workspace = r"..\..\..\Data\Canada\province.shp"
+#fc = r"C:\Users\mie_r\Documents\acgis\gis4207_Customization_1\day02\lab\Data\Canada\province.shp"
+dsFc = arcpy.Describe(env.workspace)
 
 print "BaseName : " + dsFc.BaseName
 print "CatalogPath : " + dsFc.CatalogPath
--- EmilieRandHollyL/Describe02.py
+++ EmilieRandHollyL/Describe02.py
@@ -4,11 +4,11 @@ from arcpy import env
 
 scriptPath = os.path.dirname(__file__)
 os.chdir(scriptPath)
-env.workspace = r"..\..\..\..\acgis\gis4207_Customization_1\day02\lab\Data\Canada"
-fc = r"C:\Users\mie_r\Documents\acgis\gis4207_Customization_1\day02\lab\Data\Canada\province.shp"
-dsFc = arcpy.Describe(fc)
-fmt = "%-23s: %s"
 
-print fmt % ("BaseName", dsFc.BaseName)
-print fmt % ("CatalogPath", dsFc.CatalogPath)
-print fmt % ("DataType", dsFc.DataType)
+fc = r"..\..\..\Data\Canada\province.shp"
+dsFc = arcpy.Describe(fc)
+fmt = "{:13}: {}"
+
+print fmt.format ("BaseName", dsFc.BaseName)
+print fmt.format ("CatalogPath", dsFc.CatalogPath)
+print fmt.format ("DataType", dsFc.DataType)
