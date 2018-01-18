1d650d0d2492319719e9bcddee87a081d876512e
Emilie Rabeau <rabe0019@algonquinlive.com>
2018-01-18 14:08:44 -0500

    tried describe05

--- EmilieRandHollyL/Describe05.py
+++ EmilieRandHollyL/Describe05.py
@@ -1,29 +1,35 @@
+#-------------------------------------------------------------------------------
+# Name:        Describe04
+# Purpose:
+#
+# Author:      holly long, emilie rabeau
+#
+# Created:     18-01-2018
+# Copyright:   (c) holly 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 import sys
 arcpy = None
-
-scriptPath = os.path.dirname(__file__)
-os.chdir(scriptPath)
-
-
-def main():
-
-    if arcpy.Exists():
-        print fmt.format ("BaseName", dsFc.BaseName)
-        print fmt.format ("CatalogPath", dsFc.CatalogPath)
-        print fmt.format ("DataType", dsFc.DataType)
-    else:
-        print fc + "does not exists"
-
-        setArcPy()
 
 def setArcPy():
     global arcpy
     if arcpy == None:
         import arcpy
 
-        env.workspace = r"..\..\..\Data\Canada\province.shp"
-        dsEx = arcpy.Exists(env.workspace)
-        dsFc = arcpy.Describe(env.workspace)
+def main():
+
+    if len(sys.argv) != 2:
+        print "Usage:  Describe03.py <FeatureClassName>"
+
+    else:
+        setArcPy()
+        fc=sys.argv[1]
+        dsEx.arcpy.Exists(fc)
+        dsFc=arcpy.Describe(fc)
+        fmt = "{:13}: {}"
+        print fmt.format ("BaseName", dsFc.BaseName)
+        print fmt.format ("CatalogPath", dsFc.CatalogPath)
+        print fmt.format ("DataType", dsFc.DataType)
 
 if __name__=="__main__":
     main()
