7ad4ec25616b3bfaf8a04498664411ef86003a80
Emilie Rabeau <rabe0019@algonquinlive.com>
2018-01-19 10:11:07 -0500

    no message

--- EmilieRandHollyL/List03.py
+++ EmilieRandHollyL/List03.py
@@ -1,3 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        List03
+# Purpose:
+#
+# Author:      holly long, emilie rabeau
+#
+# Created:     18-01-2018
+# Copyright:   (c) holly 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 import arcpy
 from arcpy import env
 
