48e20dd7b11e75d5829ff08c85620e947fc51c2e
Emilie Rabeau <rabe0019@algonquinlive.com>
2018-01-22 16:42:35 -0500

    List08.py has not been completed!

---
+++ EmilieRandHollyL/List08.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Name:        List08
+# Purpose:
+#
+# Author:      holly long, emilie rabeau
+#
+# Created:     18-01-2018
+# Copyright:   (c) holly 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+# Command Line Arugument used: ..\..\..\Data
+
+import os
+import arcpy
+
+rootFolder = r"..\..\..\Data"
+fileList = []
+
+
+if os.path.exists(rootFolder):
+    print "Usage:  List08.py <RootFolder>"
+
+    arcpy.env.workspace=sys.argv[1]
+
+    if arcpy.Exists(sys.argv[1]):
+        fwork = arcpy.ListWorkspaces()
+        for f in fwork:
+            print f
+
+    for root, dirs, files in os.walk(rootFolder):
+        for i in files:
+            if i.find(".shp") >= 0:
+                print os.path.join(rootFolder, i)
+
+else:
+    print "{} does not exists".format(rootFolder)
