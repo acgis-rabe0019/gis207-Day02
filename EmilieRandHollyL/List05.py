86d0b83f385fdabba13d41ed9adfdde16d20c116
Emilie Rabeau <rabe0019@algonquinlive.com>
2018-01-22 12:37:47 -0500

    haven't really done anything for List05.py

---
+++ EmilieRandHollyL/List05.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------
+# Name:        List05
+# Purpose:
+#
+# Author:      holly long, emilie rabeau
+#
+# Created:     18-01-2018
+# Copyright:   (c) holly 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import sys
+arcpy = None
+
+def setArcPy():
+    global arcpy
+    if arcpy == None:
+        import arcpy
+
+
+if len(sys.argv) != 2:
+    print "Usage:  List05.py <FeatureClassName>"
+    sys.exit()
+
+else:
+    setArcPy()
+
+    arcpy.env.workspace=sys.argv[1]
+
+    if arcpy.Exists(sys.argv[1]):
+        fwork = arcpy.ListWorkspaces()
+        for f in fwork:
+            print f
+
+    else:
+        print "{} does not exists".format(sys.argv[1])
