f2d83abd79e7f9d613f480f09e3764a91b55045d
Emilie Rabeau <rabe0019@algonquinlive.com>
2018-01-22 20:14:23 -0500

    List08 does not work!

--- EmilieRandHollyL/List08.py
+++ EmilieRandHollyL/List08.py
@@ -11,26 +11,41 @@
 # Command Line Arugument used: ..\..\..\Data
 
 import os
-import arcpy
+import sys
+arcpy=None
 
-rootFolder = r"..\..\..\Data"
-fileList = []
+def setArcPy():
+    global arcpy
+    if arcpy == None:
+        import arcpy
 
-
-if os.path.exists(rootFolder):
-    print "Usage:  List08.py <RootFolder>"
-
-    arcpy.env.workspace=sys.argv[1]
-
-    if arcpy.Exists(sys.argv[1]):
-        fwork = arcpy.ListWorkspaces()
-        for f in fwork:
-            print f
-
-    for root, dirs, files in os.walk(rootFolder):
-        for i in files:
-            if i.find(".shp") >= 0:
-                print os.path.join(rootFolder, i)
+if len(sys.argv) != 2:
+    print "Usage: List08.py <RootFolder>"
+    sys.exit()
 
 else:
-    print "{} does not exists".format(rootFolder)
+    setArcPy()
+
+    arcpy.env.workspace = sys.argv[1]
+
+    if arcpy.Exists(sys.argv[1]):
+        for root, dirs, files in os.walk(sys.argv[1]):
+            arcpy.env.workspace = root
+            workspaces = arcpy.ListWorkspaces('*','All')
+
+            for workspace in workspaces:
+                print os.path.abspath(workspace)
+
+                arcpy.env.workspace = os.path.abspath(workspace)
+                fc = arcpy.ListFeatureClasses()
+
+                for feature_class in fc:
+                    print os.path.join(workspace, feature_class)
+                print '\n'
+
+                fileOut = open(r"C:\Users\mie_r\Documents\acgis\gis4207_Customization_1\Data\outputfilename.txt", 'w')
+                fileOut.write(ww)
+                file.close
+
+    else:
+        print "{} does not exists".format(root)
