1343849baa0eb3d9e5686203d01c2723af565896
Emilie Rabeau <rabe0019@algonquinlive.com>
2018-01-23 22:36:50 -0500

    the output from List08 finally prints in the .txt file !!!

--- EmilieRandHollyL/List08.py
+++ EmilieRandHollyL/List08.py
@@ -21,6 +21,7 @@ def setArcPy():
         import arcpy
 
 if len(sys.argv) != 3:
+
     print "Usage: List08.py <RootFolder>"
     sys.exit()
 
@@ -30,24 +31,24 @@ else:
     arcpy.env.workspace = sys.argv[1]
 
     if arcpy.Exists(sys.argv[1]):
-        outFileName=open(sys.argv[2], 'w')
+        outFileName=open(sys.argv[2], 'w+')
 
         for root, dirs, files in os.walk(sys.argv[1]):
             arcpy.env.workspace = root
             workspaces = arcpy.ListWorkspaces('*','All')
 
             for workspace in workspaces:
-                info1=os.path.abspath(workspace)
-                outFileName.write(info1, "\n")
+                info1 = os.path.abspath(workspace)
+                outFileName.write(info1 + "\n")
 
                 arcpy.env.workspace = os.path.abspath(workspace)
                 fc = arcpy.ListFeatureClasses()
 
                 for feature_class in fc:
                     info2=os.path.join(workspace, feature_class)
-                    outFileName.write(info2,"\n")
+                    outFileName.write(info2 +"\n")
 
-        outFileName.write("\n")
+                    outFileName.write("\n")
         outFileName.close()
     else:
         print "{} does not exists".format(root)
